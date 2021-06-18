fadc7eb71ff41c54f15150c0e87ed9585e4efc3e
Colin Woodbury <colin@fosskers.ca>
2021-06-18 08:40:34 -0700

    Initial commit

---
+++ streak.el
@@ -0,0 +1,25 @@
+;;; streak.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Colin Woodbury
+;;
+;; Author: Colin Woodbury <https://github.com/fosskers>
+;; Maintainer: Colin Woodbury <colin@fosskers.ca>
+;; Created: June 18, 2021
+;; Modified: June 18, 2021
+;; Version: 0.0.1
+;; Keywords: Symbol’s value as variable is void: finder-known-keywords
+;; Homepage: https://github.com/fosskers/streak
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'streak)
+;;; streak.el ends here
