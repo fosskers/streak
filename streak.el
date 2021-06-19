cd1d7a5aae324fa1d53c1d429b358a30f2b4fcdf
Colin Woodbury <colin@fosskers.ca>
2021-06-18 18:07:59 -0700

    First pass at logic
    Time for dinner!

--- streak.el
+++ streak.el
@@ -19,7 +19,72 @@
 ;;
 ;;; Code:
 
+(define-minor-mode streak-mode
+  "Display the number of days of a successful streak in the mode line."
+  :init-value nil
+  :global t
+  :group 'streak
+  :lighter " Streak")
 
+(defvar streak--seconds-per-day 86400
+  "The number of seconds in a day.")
+
+(defcustom streak-file (substitute-in-file-name "$HOME/.streak")
+  "The location to save the start of the current streak."
+  :group 'streak
+  :type 'file)
+
+(defun streak--days-since-unix-epoch ()
+  "The number of days since the Unix Epoch."
+  (/ (time-convert nil 'integer)
+     streak--seconds-per-day))
+
+(defun streak--read-streak-start ()
+  "Read the starting date of the current streak."
+  (unless (file-exists-p streak-file)
+    (streak-init))
+  (when-let ((buffer (find-file-noselect streak-file)))
+    (with-current-buffer buffer
+      (let* ((raw (progn
+                    (copy-region-as-kill (line-beginning-position) (line-end-position))
+                    (current-kill 0)))
+             (start (string-to-number raw))
+             (today (streak--days-since-unix-epoch))
+             (delta (- today start)))
+        (if (= 1 delta)
+            "1 Day"
+          (format "%d Days" delta))))))
+
+(defun streak-init ()
+  "Mark today as the start of a new streak."
+  (interactive)
+  (when-let ((buffer (find-file-noselect streak-file))
+             (today (streak--days-since-unix-epoch)))
+    (with-current-buffer buffer
+      (message "Streak: Setting your first streak!")
+      (delete-region (point-min) (point-max))
+      (insert (format "%d\n" today))
+      (save-buffer))))
+
+(defun streak-reset ()
+  "Reset the streak back to 0."
+  (interactive)
+  (when-let ((buffer (find-file-noselect streak-file)))
+    (with-current-buffer buffer
+      (message "Streak: Resetting your streak. Restart Emacs to see the effect.")
+      (delete-region (point-min) (point-max))
+      (insert (streak--tomorrow))
+      (save-buffer))))
+
+(defun streak--tomorrow ()
+  "Tomorrow's date, as days since the Unix epoch."
+  (1+ (streak--days-since-unix-epoch)))
+
+(defun streak--show-streak-in-modeline ()
+  "Show the current streak count in days in the mode line."
+  (add-to-list 'global-mode-string '(t (:eval (streak--read-streak-start)))))
+
+(add-hook 'streak-mode-on-hook #'streak--show-streak-in-modeline)
 
 (provide 'streak)
 ;;; streak.el ends here
